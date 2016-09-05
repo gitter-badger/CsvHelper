7b63e864263142201cf4d4203628629cdcfa4ecf
Josh Close <narshe@gmail.com>
2016-09-05 10:06:32 -0500

    wip

--- src/CsvHelper.Tests/CsvParserRawRecordTests.cs
+++ src/CsvHelper.Tests/CsvParserRawRecordTests.cs
@@ -87,6 +87,32 @@ namespace CsvHelper.Tests
 		}
 
 		[TestMethod]
+		public void RawRecordCr2DelimiterTest()
+		{
+			using( var stream = new MemoryStream() )
+			using( var writer = new StreamWriter( stream ) )
+			using( var reader = new StreamReader( stream ) )
+			using( var parser = new CsvParser( reader ) )
+			{
+				writer.Write( "1;;2\r" );
+				writer.Write( "3;;4\r" );
+				writer.Flush();
+				stream.Position = 0;
+
+				parser.Configuration.Delimiter = ";;";
+
+				parser.Read();
+				Assert.AreEqual( "1;;2\r", parser.RawRecord );
+
+				parser.Read();
+				Assert.AreEqual( "3;;4\r", parser.RawRecord );
+
+				parser.Read();
+				Assert.AreEqual( string.Empty, parser.RawRecord );
+			}
+		}
+
+		[TestMethod]
 		public void TinyBufferTest()
 		{
 			using( var stream = new MemoryStream() )
